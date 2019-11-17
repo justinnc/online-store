f6abcc0d85c5bfb672a98b73b489b2c3d0e3b1d3
justinnc <41505603+justinnc@users.noreply.github.com>
2019-11-17 10:52:32 +0700

    add layout, component menu

--- src/App.js
+++ src/App.js
@@ -1,26 +1,11 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import HomePage from "./pages/homepage.component.jsx/homepage.component";
+class App extends Component {
+  render() {
+    return <HomePage />;
+  }
 }
 
 export default App;

a17c92daaac5aa24f6741dd1b11267d8da5ae7a7
justinnc <41505603+justinnc@users.noreply.github.com>
2019-12-12 21:45:34 +0700

    add redux persist to handle state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import { BrowserRouter } from "react-router-dom";
-
 // import Provider từ react-redux
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
+import configureStore from "./redux/store";
 
+import "./index.css";
+import App from "./App";
+
+const { store, persistor } = configureStore();
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById("root")
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,10 +2,27 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // defaults to localStorage for web
+
 import rootReducer from "./root-reducer";
+
+const persistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"]
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export default function configureStore() {
+  const store = createStore(persistedReducer, applyMiddleware(...middlewares));
 
-export default store;
+  // @NOTE not sure what this does, assuming listeners is defined outside of this method?
+
+  const persistor = persistStore(store);
+
+  return { store, persistor };
+}
